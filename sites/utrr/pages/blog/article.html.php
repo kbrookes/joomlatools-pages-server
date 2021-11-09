de125b8a2b4f56e5cd7979664080c4f2d2e4a6da
kbrookes <kelsey@lovedlockedloaded.com>
2021-11-09 09:37:18 +0000

    add video player

--- sites/utrr/pages/blog/article.html.php
+++ sites/utrr/pages/blog/article.html.php
@@ -29,6 +29,9 @@ $published = date('Y-m-d',$date);
     <div class="col-span-2 content-main">
         <span><a href="<?= route(page('/blog/index')) ?>">< GO BACK</a></span>
         <h1 class="text-primary"><?= collection()->title ?></h1>
+        <? if(!empty($video)){
+            echo '<div class="video" data-id="' . $video . '"></div>';
+        } ?>
         <? if(!empty($thumb)){?>
             <img src="<?= $thumb; ?>" />
         <? } ?>
@@ -38,3 +41,33 @@ $published = date('Y-m-d',$date);
     </div>
     
 </div>
+
+<script>
+    document.addEventListener("DOMContentLoaded",
+    function() {
+        var a, n,
+            v = document.getElementsByClassName("video");
+        for (n = 0; n < v.length; n++) {
+            a = document.createElement("div");
+            a.setAttribute("data-id", v[n].dataset.id);
+            a.innerHTML = videoThumb(v[n].dataset.id);
+            a.onclick = videoIframe;
+            v[n].appendChild(a);
+        }
+    });
+    
+    function videoThumb(id) {
+        var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+            playBtn = '<span></span>';
+        return thumb.replace("ID", id) + playBtn;
+    }
+    
+    function videoIframe() {
+        var iframe = document.createElement("iframe");
+        iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.id + "?autoplay=1");
+        iframe.setAttribute("allowfullscreen", "1");
+        iframe.setAttribute("frameborder", "0");
+        this.parentNode.replaceChild(iframe, this);
+    }
+    
+</script>
